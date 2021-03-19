3a04d74a9bb71667973ca68ee61e6494ec308e09
Barbara Diaz <barbarad91@gmail.com>
2021-03-19 13:48:36 +0100

    Add vercel regex to CORS whitelist

--- server/config/cors.config.js
+++ server/config/cors.config.js
@@ -1,10 +1,11 @@
-const cors = require("cors")
+const cors = require('cors')
 
 const whitelist = [process.env.DOMAIN]
 
 const corsOptions = {
   origin: (origin, cb) => {
-    const originIsWhitelisted = whitelist.includes(origin)
+    const vercelRegex = /https:\/\/reto-web.*\.vercel\.app/
+    const originIsWhitelisted = whitelist.includes(origin) || vercelRegex.test(origin)
     cb(null, originIsWhitelisted)
   },
   credentials: true,
